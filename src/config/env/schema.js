a7b683eea90f5f87008776206e4f5470f488da89
Juan Rodriguez Gasca <engineer.jrg@gmail.com>
2021-06-26 09:01:52 -0500

    :hammer: Configurando env para autenticacion y autorizacion

--- src/config/env/schema.js
+++ src/config/env/schema.js
@@ -18,6 +18,12 @@ module.exports = {
       default: 'AppName',
       env: 'APP_NAME',
     },
+    secret_key: {
+      doc: 'Llave secreta para realkizar encriptacion.',
+      format: String,
+      default: 'jg8dsyu4kf0sedshf67sg68f8hbnjttj',
+      env: 'APP_SECRET_KEY',
+    },
   },
   db: {
     dialect: {
