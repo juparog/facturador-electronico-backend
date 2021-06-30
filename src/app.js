15e6ecbbcdb413c792ffb0e25f248c8734ccc65a
EfrainRodriguez <efrainrg92@gmail.com>
2021-06-30 19:52:28 -0300

    :hammer: agregando middleware de cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
@@ -7,6 +8,7 @@ import { configEnv } from './config/env/config';
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
