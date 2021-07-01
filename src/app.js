c83c3d5634282a2d9b76a421735412da4036a061
EfrainRodriguez <efrainrg92@gmail.com>
2021-07-01 15:01:14 -0300

    Configurando swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 import indexRouter from './routes/index';
 import { dbSync } from './helpers/db';
 import { configEnv } from './config/env/config';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Facturador Electrónico - API',
+      version: '1.0.0',
+      description: 'Esta API pertenece al proyecto del facturador electrónico',
+    },
+    servers: [
+      {
+        url: 'https://facturador-electronico-api.herokuapp.com/api',
+      },
+    ],
+  },
+  apis: [ './routes/index.js' ]
+};
+
+const specs = swaggerJSDoc(options);
 
 const app = express();
 
@@ -12,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api', indexRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 dbSync(JSON.parse(JSON.stringify(configEnv.get('db.sync'))));
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,47 @@ import { getListUser, createUser, updateUser } from '../controllers';
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    users:
+ *      type: object
+ *      properties:
+ *        nit:
+ *          type: string
+ *          description: numero de cédula del usuario con al menos 7 dígitos
+ *        email:
+ *          type: string
+ *          description: email de acceso del usuario
+ *        password:
+ *          type: string
+ *          description: contraseña alfanumérica de mínimo 8 caracteres
+ *      required:
+ *        - nit
+ *        - email
+ *        - password
+ *      example:
+ *        nit: 1234567
+ *        email: email@email.com
+ *        password: password123
+ */
+
 // Get /api/users
+/**
+ * /api/users:
+ *  get:
+ *    summary: Retorna la lista de usuarios registrados
+ *    responses:
+ *      200:
+ *        description: Lista de usuarios
+ *        constent:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/users'
+ */
 router.get('/', getListUser);
 
 // Post /api/users
