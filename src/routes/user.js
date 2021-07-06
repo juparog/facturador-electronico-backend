a507a136d049f40a82a107a58d553755321c99f8
Juan Rodriguez Gasca <engineer.jrg@gmail.com>
2021-07-06 16:08:26 -0500

    :books: Iniciando la documentacion de la api - swagger

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,36 +5,60 @@ const router = Router();
 
 /**
  * @swagger
- * components:
- *  schemas:
- *    users:
- *      type: object
- *      properties:
- *        nit:
- *          type: string
- *          description: numero de cédula del usuario con al menos 7 dígitos
- *        email:
- *          type: string
- *          description: email de acceso del usuario
- *        password:
- *          type: string
- *          description: contraseña alfanumérica de mínimo 8 caracteres
- *      required:
- *        - nit
- *        - email
- *        - password
- *      example:
- *        nit: 1234567
- *        email: email@email.com
- *        password: password123
+ * definitions:
+ *   User:
+ *     type: object
+ *     required:
+ *       - firstName
+ *       - lastName
+ *       - username
+ *       - password
+ *       - email
+ *       - nit
+ *       - state
+ *     properties:
+ *       id:
+ *         type: integer
+ *         description: Id del usuario.
+ *         example: 1
+ *       firstName:
+ *         type: string
+ *         description: Nombre del usuario.
+ *         example: Lindo
+ *       lastName:
+ *         type: string
+ *         description: Apellido del usuario.
+ *         example: Hermoso
+ *       username:
+ *         type: string
+ *         description: Usuario.
+ *         example: lindohermoso
+ *       password:
+ *         type: string
+ *         description: Contraseña del usuario.
+ *         example: repollo123
+ *       email:
+ *         type: string
+ *         description: Correo del usuario.
+ *         example: lindohermoso@email.com
+ *       nit:
+ *         type: string
+ *         description: Nit del usuario.
+ *         example: 1234567890
+ *       state:
+ *         type: string
+ *         description: Estado del usuario.
+ *         example: ACTIVE
  */
 
 /**
  * @swagger
- * /users:
+ * /api/users:
  *   get:
  *     summary: Recupera una lista de usuarios.
- *     description: Recupera una lista de usuarios. Se pueden enviar querys para filtrar ó ordenar los datos a recuperar, tambien como para paginarlos.
+ *     description: Recupera una lista de usuarios.
+ *     tags:
+ *       - Usuarios
  *     responses:
  *       200:
  *         description: Una lista de usuarios.
@@ -46,50 +70,140 @@ const router = Router();
  *                 data:
  *                   type: array
  *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         description: Id del usuario.
- *                         example: 1
- *                       firstName:
- *                         type: string
- *                         description: Nombre del usuario.
- *                         example: Lindo
- *                       lastName:
- *                         type: string
- *                         description: Apellido del usuario.
- *                         example: Hermoso
- *                       username:
- *                         type: string
- *                         description: Usuario.
- *                         example: lindohermoso
- *                       password:
- *                         type: string
- *                         description: Contraseña del usuario.
- *                         example: repollo123
- *                       email:
- *                         type: string
- *                         description: Correo del usuario.
- *                         example: lindohermoso@email.com
- *                       nit:
- *                         type: string
- *                         description: Nit del usuario.
- *                         example: 1234567890
- *                       state:
- *                         type: string
- *                         description: Estado del usuario.
- *                         example: ACTIVE
+ *                     $ref: '#/definitions/User'
+ *       409:
+ *         description: Una estructura con informacion de los inconvenientes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensage de informacion.
+ *                   example: No se pudo obtener una lista de usuarios
  */
 router.get('/', getListUser);
 
-// Post /api/users
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Crea un usuario.
+ *     description: Crea un usuario en la base de datos.
+ *     tags:
+ *       - Usuarios
+ *     parameters:
+ *     - in: body
+ *       name: user
+ *       description: El usuario para crear.
+ *       schema:
+ *         $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: El suario que se creó.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/definitions/User'
+ *       409:
+ *         description: Una estructura con informacion de los inconvenientes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensage de informacion.
+ *                   example: No se pudo crear el usuario.
+ */
 router.post('/', createUser);
 
-// Put /api/users/:id
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Actualizar un usuario.
+ *     description: Actualizar un usuario en la base de datos.
+ *     tags:
+ *       - Usuarios
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        description: Id numerico del usuario que se va a actualizar
+ *        schema:
+ *          type: integer
+ *        required: true
+ *      - in: body
+ *        name: user
+ *        description: Los campos del usuario para actualizar.
+ *        schema:
+ *          $ref: '#/definitions/User'
+ *     responses:
+ *       200:
+ *         description: El usuario con los campos actualizados.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/definitions/User'
+ *       409:
+ *         description: Una estructura con informacion de los inconvenientes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensage de informacion.
+ *                   example: No se pudo actualizar el usuario.
+ */
 router.put('/:id', updateUser);
 
-// Delete /api/users/:id
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Eliminar un usuario.
+ *     description: Eliminar un usuario en la base de datos (desactivar).
+ *     tags:
+ *       - Usuarios
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        description: Id numerico del usuario que se va a eliminar
+ *        schema:
+ *          type: integer
+ *        required: true
+ *     responses:
+ *       200:
+ *         description: El usuario con los campos actualizados.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: '#/definitions/User'
+ *       409:
+ *         description: Una estructura con informacion de los inconvenientes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensage de informacion.
+ *                   example: No se pudo actualizar el usuario.
+ */
 router.delete('/:id', updateUser);
 
 export { router };
