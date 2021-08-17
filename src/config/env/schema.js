1f1705b1f0dafdb1de29374b97311e55d772d9ee
Juan Rodriguez Gasca <engineer.jrg@gmail.com>
2021-08-16 21:57:16 -0500

    :hammer: Se agrego las variables de sendgrid email al esquema de convict

--- src/config/env/schema.js
+++ src/config/env/schema.js
@@ -158,7 +158,19 @@ module.exports = {
       doc: 'Ruta para restablecer la contraseña',
       format: String,
       default: 'http://localhost',
-      env: 'APP_SMTP_RESET_PASSWORD',
+      env: 'SMTP_RESET_PASSWORD',
+    },
+    templateId: {
+      doc: 'Id del template en email sendgrid/mail',
+      format: String,
+      default: '1-myTemplateId1234',
+      env: 'SMTP_TEMPLATE_ID_SENDGRID',
+    },
+    sendGridApiKey: {
+      doc: 'Id del template en email sendgrid/mail',
+      format: String,
+      default: 'apyKeysengrid-email12746',
+      env: 'SMTP_API_KEY_SENDGRID',
     },
   },
 };
